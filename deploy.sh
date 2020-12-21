10a2a36158e8a641ecc747019679b2dcfaa6bf90
Pavel Ivanov <pavel.ivanov.developer@gmail.com>
2020-12-21 15:40:47 +0200

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,10 @@
-npm i -g @angular/cli@v8
-npm i -g typescript
-
 cd frontend
 npm i
 npm run build
 cd ..
 mkdir -p backend/public
 rm -rf backend/public
-cp -r frontent/dist/frontent backend/public
+cp -r frontend/dist/frontend backend/public
 cd backend
 npm i
 npm run build
